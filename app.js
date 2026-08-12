668c8030b1f336ddb3eb13c10762377ccb8342f7
Husna <husnaali8uni@gmail.com>
2026-08-12 18:31:45 +0300

    refactor:change how the sidebar will look like and added to the css

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('test seenjeem')
+/*-------------------------------- Constants --------------------------------*/
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let selectedCategories=[]
+
+
+/*------------------------ Cached Element References ------------------------*/
+const bahrainq1Element = document.querySelectorAll('.bahrain-q1')
+const bahrainq2Element = document.querySelectorAll('.bahrain-q2')
+const bahrainq2Element = document.querySelectorAll('.bahrain-q3')
+const bahrainq2Element = document.querySelectorAll('.bahrain-q4')
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
